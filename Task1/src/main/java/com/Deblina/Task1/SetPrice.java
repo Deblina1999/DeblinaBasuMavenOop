6ea6a8362c2aa1df529fb8bf529cb3e78f7e6f7e
Deblina1999 <56560749+Deblina1999@users.noreply.github.com>
2020-02-08 22:30:27 +0530

    Add files via upload
    Maven Project 1

---
+++ Task1/src/main/java/com/Deblina/Task1/SetPrice.java
@@ -0,0 +1,21 @@
+package com.Deblina.Task1;
+
+public class SetPrice 
+{
+	private int fromRange;
+	private int toRange;
+	public int getFromRange() {
+		return fromRange;
+	}
+	public void setFromRange(int fromRange) {
+		this.fromRange = fromRange;
+	}
+	public int getToRange() {
+		return toRange;
+	}
+	public void setToRange(int toRange) {
+		this.toRange = toRange;
+	}
+	
+}
+
